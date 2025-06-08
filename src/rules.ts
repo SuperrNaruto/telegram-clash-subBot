1ff4bc8fbd0cec25c4bc1bfd7103b4f8b943766b
yuki <100200996+SuperrNaruto@users.noreply.github.com>
2025-06-08 22:08:12 +0800

    Update rules.ts

--- src/rules.ts
+++ src/rules.ts
@@ -4,12 +4,12 @@ import { alias } from "./alias";
 const BASE =
   "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash";
 
-  export async function fetchRule(app: string, token?: string): Promise<string> {
-    const folder = alias[app] ?? app;
-      const url = `${BASE}/${folder}/${folder}.yaml`;
-        const { data } = await axios.get(url, {
-            headers: token ? { Authorization: `token ${token}` } : undefined,
-                timeout: 15000
-                  });
-                    return data as string;
-                }
+export async function fetchRule(app: string, token?: string): Promise<string> {
+  const folder = alias[app] ?? app;
+  const url = `${BASE}/${folder}/${folder}.yaml`;
+  const { data } = await axios.get(url, {
+    headers: token ? { Authorization: `token ${token}` } : undefined,
+    timeout: 15000
+  });
+  return data as string;
+}
